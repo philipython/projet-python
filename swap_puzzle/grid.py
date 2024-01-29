f4fa6b5bbeaf3651f3df18e37488bed03cfa80ef
mmarsot-ensae <mila.marsot@ensae.fr>
2024-01-29 08:57:05 +0000

    sf

--- swap_puzzle/grid.py
+++ swap_puzzle/grid.py
@@ -77,7 +77,7 @@ class Grid():
         """
         i1, j1 = cell1
         i2, j2 = cell2
-        if is_sorted([self.state[i1][j1], self.state[i2][j2]])==True:
+        if is_sorted([self[cell1], self[cell2]])==True:
             self.state[i1][j1], self.state[i2][j2] = self.state[i2][j2], self.state[i1][j1]
             
 
