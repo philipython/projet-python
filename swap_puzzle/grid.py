308b79463c8544eaf67b847fbfa9b36e193856ec
pseilles-ensae <philipa.seilles@ensae.fr>
2024-01-29 08:22:36 +0000

    swap

--- swap_puzzle/grid.py
+++ swap_puzzle/grid.py
@@ -69,8 +69,12 @@ class Grid():
         cell1, cell2: tuple[int]
             The two cells to swap. They must be in the format (i, j) where i is the line and j the column number of the cell. 
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
-        raise NotImplementedError
+        if self[cell1]==self[cell2+(0,1)] or self[cell1]==self[cell2+(1,0)] or self[cell1]==self[cell2+(-1,0)] or self[cell1]==self[cell2+(0,-1)]:
+            b=cell1
+            c=cell2
+            cell2=b
+            cell1=c
+        return(self) 
 
     def swap_seq(self, cell_pair_list):
         """
@@ -82,8 +86,7 @@ class Grid():
             List of swaps, each swap being a tuple of two cells (each cell being a tuple of integers). 
             So the format should be [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...].
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
-        raise NotImplementedError
+        
 
     @classmethod
     def grid_from_file(cls, file_name): 
