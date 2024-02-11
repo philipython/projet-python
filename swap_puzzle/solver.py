e0b8dbfb825b0585467c8f9db179ed8608bd41c9
philipython <156650979+philipython@users.noreply.github.com>
2024-02-11 21:31:31 +0100

    Update solver.py

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -12,46 +12,46 @@ class Solver():
         Solves the grid and returns the sequence of swaps at the format 
         [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...]. 
         """
-        # 1. Find the element
-        def find_element(element, grid):
+        # 1. première étape : on veut connaître la position (coordonnées) de telle ou telle case
+        def trouver_élément(element, grid):
             for i in range(grid.m):
                 for j in range(grid.n):
                     if grid.state[i][j] == element:
                         return (i, j)
                         
-        # 2. For each line, give it the elements it should contain
-        def align_element_in_line(line, current_column, shift, list_moves = []):
+        # 2. deuxième étape : pour chaque ligne, la fonction donne les éléments qu'elle doit contenir
+        def aligner_element(line, current_column, shift, list_moves = []):
             if shift == 0:
                 return list_moves
             else:
                 if shift >= 1:
                     line[current_column], line[current_column + 1] = line[current_column + 1], line[current_column]
                     list_moves.append(((line, current_column),(line, current_column + 1)))
-                    return align_element_in_line(line, current_column + 1, shift - 1, list_moves)
+                    return aligner_element(line, current_column + 1, shift - 1, list_moves)
                 if shift <= -1:
                     line[current_column-1], line[current_column] = line[current_column], line[current_column-1]
                     list_moves.append(((line, current_column),(line, current_column - 1)))
-                    return align_element_in_line(line, current_column - 1, shift + 1, list_moves)"
+                    return aligner_element(line, current_column - 1, shift + 1, list_moves)"
                     
                     
         def get_elements_for_line(grid, line_index,):
             """Brings the elements [line*self.grid.n, ... (line+1)*self.grid.n - 1] on the line line_index."""
             big_list_moves = []
-            # The line has elements [line*self.grid.n, ... (line+1)*self.grid.n - 1]
-            list_elements = list(range(line_index*grid.n+1, (line_index+1)*grid.n+1)) # The numbers start at 1
+            # la ligne a des éléments [line*self.grid.n, ... (line+1)*self.grid.n - 1]
+            list_elements = list(range(line_index*grid.n+1, (line_index+1)*grid.n+1)) # les nombres commencent à 1
             print(f\"{list_elements=}\")
-            #  All elements that are supposed to be on this line
+            #  tous les éléments qui sont censés être sur cette ligne
             for element in list_elements:
-                # Find where it is.
-                l_good, c_good = find_element(element, grid)
+                # trouver où il est
+                l_good, c_good = trouver_élément(element, grid)
                 if l_good != line_index:
-                    # Look for a column in line line_index that contains a bad element
+                    # on cherche ensuite une colonne dans line_index qui contient un élément qui ne correspond pas
                     for column, element in enumerate(grid.state[line_index]):
                         if not element in list_elements:
                             c_bad = column
-                            break # Get out of the for loop when we find one.
-                    # Now we have the column of a bad element in line_index. Move the element at position i, j to position i, column_bad_element.
-                    big_list_moves += align_element_in_line(grid.state[l_good], c_good, c_bad-c_good)
+                            break # on sort de la boucle for quand on en trouve un
+                    # on a maintenant une colonne avec un mauvais élément dans line_index. On veut bouger cet élément de la position i, j à la position i, column_bad_element.
+                    big_list_moves += aligner_element(grid.state[l_good], c_good, c_bad-c_good)
                     if l_good > line_index:
                         sign = -1
                     else:
